18debeb7d3d6e2d7744eba13485275ab3798e90c
edgar alfredo gonzalez salazar <piligonio@gmail.com>
2018-10-12 13:20:24 -0500

    Ultimos cambios

---
+++ EleventaNTierLayerV2/EleventaNTierLayerV2.BusinessEntities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleventaNTierLayerV2.BusinessEntities
+{
+    public class Class1
+    {
+    }
+}
